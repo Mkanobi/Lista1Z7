10f97a911499a7ad1c9b7d5318d5d75da9dede5b
mkanobi-ssh <mkanobi@ssh>
2021-10-17 20:09:16 +0200

    Dodano komentarze do Input.java

--- calculator/src/main/java/calculator/Input.java
+++ calculator/src/main/java/calculator/Input.java
@@ -9,84 +9,98 @@ import java.util.*;
 
 public class Input extends JPanel implements ActionListener {
 
+    /** Referencja na logikę kalkulatora. */
     private TheCalculator calculator;
+    /** Referencja na output. */
     private Output output;
 
-    private ArrayList<CButton> B;
-    private CButton Bplus;
-    private CButton Bminus;
-    private CButton Bdiv;
-    private CButton Bmult;
-    private CButton Beq;
-    private CButton Besc;
-
-    public Input(TheCalculator calculator, Output output) {
+    /** Tablica przycisków cyfr. */
+    private ArrayList<CButton> b;
+    /** Przycisk "+". */
+    private CButton bplus;
+    /** Przycisk "-". */
+    private CButton bminus;
+    /** Przycisk "/". */
+    private CButton bdiv;
+    /** Przycisk "*". */
+    private CButton bmult;
+    /** Przycisk "=". */
+    private CButton beq;
+    /** Przycisk "<-". */
+    private CButton besc;
+    /** Constructor inputu.
+     * @param calculator referencja na kalkulator
+     * @param output referencja na output
+     * */
+    public Input(final TheCalculator calculator, final Output output) {
         
-        B = new ArrayList<CButton>();
+        b = new ArrayList<CButton>();
         for (int i = 0; i <= 9; i++) {
-            B.add(new CButton(Integer.toString(i)));
+            b.add(new CButton(Integer.toString(i)));
         }
         this.calculator = calculator;
         this.output = output;
-        Bplus = new CButton("+");
-        Bminus = new CButton("-");
-        Bmult = new CButton("*");
-        Bdiv = new CButton("/");
-        Besc = new CButton("<-");
-        Beq = new CButton("=");
+        bplus = new CButton("+");
+        bminus = new CButton("-");
+        bmult = new CButton("*");
+        bdiv = new CButton("/");
+        besc = new CButton("<-");
+        beq = new CButton("=");
         this.setLayout(new GridLayout(4, 4));
-        this.add(B.get(0));
-        this.add(B.get(1));
-        this.add(B.get(2));
-        this.add(Besc);
-        this.add(B.get(3));
-        this.add(B.get(4));
-        this.add(B.get(5));
-        this.add(Bplus);
-        this.add(B.get(6));
-        this.add(B.get(7));
-        this.add(B.get(8));
-        this.add(Bminus);
-        this.add(B.get(9));
-        this.add(Bdiv);
-        this.add(Bmult);
-        this.add(Beq);
-        B.get(0).addActionListener(this);
+        this.add(b.get(0));
+        this.add(b.get(1));
+        this.add(b.get(2));
+        this.add(besc);
+        this.add(b.get(3));
+        this.add(b.get(4));
+        this.add(b.get(5));
+        this.add(bplus);
+        this.add(b.get(6));
+        this.add(b.get(7));
+        this.add(b.get(8));
+        this.add(bminus);
+        this.add(b.get(9));
+        this.add(bdiv);
+        this.add(bmult);
+        this.add(beq);
+        b.get(0).addActionListener(this);
         for (int i = 0; i <= 9; i++) {
-            B.get(i).addActionListener(this);
+            b.get(i).addActionListener(this);
         }
-        Bplus.addActionListener(this);
-        Bminus.addActionListener(this);
-        Bmult.addActionListener(this);
-        Bdiv.addActionListener(this);
-        Besc.addActionListener(this);
-        Beq.addActionListener(this);
+        bplus.addActionListener(this);
+        bminus.addActionListener(this);
+        bmult.addActionListener(this);
+        bdiv.addActionListener(this);
+        besc.addActionListener(this);
+        beq.addActionListener(this);
 
     }
-
-    public void actionPerformed(ActionEvent e) {
+    /** Funkcja od ActionListener.
+     * @param e wykonywana akcja(przycisk)
+     *  */
+    public void actionPerformed(final ActionEvent e) {
         Object source = e.getSource();
         for (int i = 0; i <= 9; i++) {
-            if (source == B.get(i)) {
+            if (source == b.get(i)) {
                 calculator.inputCurrent(i);
             }
         }
-        if (source == Bplus) {
+        if (source == bplus) {
             calculator.inputOperation('+');
         }
-        if (source == Bminus) {
+        if (source == bminus) {
             calculator.inputOperation('-');
         }
-        if (source == Bmult) {
+        if (source == bmult) {
             calculator.inputOperation('*');
         }
-        if (source == Bdiv) {
+        if (source == bdiv) {
             calculator.inputOperation('/');
         }
-        if (source == Beq) {
+        if (source == beq) {
             calculator.equals();
         }
-        if (source == Besc) {
+        if (source == besc) {
             calculator.delete();
         }
 
