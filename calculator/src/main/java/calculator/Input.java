244af320a29948612d2ab7b238b3ae98e5907ac7
mkanobi-ssh <mkanobi@ssh>
2021-10-17 20:00:03 +0200

    Zastąpiono 10 przycisków tablicą dziesięciu przycisków

--- calculator/src/main/java/calculator/Input.java
+++ calculator/src/main/java/calculator/Input.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import javax.swing.JPanel;
+import java.util.*;
 
 
 public class Input extends JPanel implements ActionListener {
@@ -11,16 +12,7 @@ public class Input extends JPanel implements ActionListener {
     private TheCalculator calculator;
     private Output output;
 
-    private CButton B0;
-    private CButton B1;
-    private CButton B2;
-    private CButton B3;
-    private CButton B4;
-    private CButton B5;
-    private CButton B6;
-    private CButton B7;
-    private CButton B8;
-    private CButton B9;
+    private ArrayList<CButton> B;
     private CButton Bplus;
     private CButton Bminus;
     private CButton Bdiv;
@@ -29,19 +21,13 @@ public class Input extends JPanel implements ActionListener {
     private CButton Besc;
 
     public Input(TheCalculator calculator, Output output) {
-
+        
+        B = new ArrayList<CButton>();
+        for (int i = 0; i <= 9; i++) {
+            B.add(new CButton(Integer.toString(i)));
+        }
         this.calculator = calculator;
         this.output = output;
-        B0 = new CButton("0");
-        B1 = new CButton("1");
-        B2 = new CButton("2");
-        B3 = new CButton("3");
-        B4 = new CButton("4");
-        B5 = new CButton("5");
-        B6 = new CButton("6");
-        B7 = new CButton("7");
-        B8 = new CButton("8");
-        B9 = new CButton("9");
         Bplus = new CButton("+");
         Bminus = new CButton("-");
         Bmult = new CButton("*");
@@ -49,32 +35,26 @@ public class Input extends JPanel implements ActionListener {
         Besc = new CButton("<-");
         Beq = new CButton("=");
         this.setLayout(new GridLayout(4, 4));
-        this.add(B0);
-        this.add(B1);
-        this.add(B2);
+        this.add(B.get(0));
+        this.add(B.get(1));
+        this.add(B.get(2));
         this.add(Besc);
-        this.add(B3);
-        this.add(B4);
-        this.add(B5);
+        this.add(B.get(3));
+        this.add(B.get(4));
+        this.add(B.get(5));
         this.add(Bplus);
-        this.add(B6);
-        this.add(B7);
-        this.add(B8);
+        this.add(B.get(6));
+        this.add(B.get(7));
+        this.add(B.get(8));
         this.add(Bminus);
-        this.add(B9);
+        this.add(B.get(9));
         this.add(Bdiv);
         this.add(Bmult);
         this.add(Beq);
-        B0.addActionListener(this);
-        B1.addActionListener(this);
-        B2.addActionListener(this);
-        B3.addActionListener(this);
-        B4.addActionListener(this);
-        B5.addActionListener(this);
-        B6.addActionListener(this);
-        B7.addActionListener(this);
-        B8.addActionListener(this);
-        B9.addActionListener(this);
+        B.get(0).addActionListener(this);
+        for (int i = 0; i <= 9; i++) {
+            B.get(i).addActionListener(this);
+        }
         Bplus.addActionListener(this);
         Bminus.addActionListener(this);
         Bmult.addActionListener(this);
@@ -86,35 +66,10 @@ public class Input extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
-        if (source == B0) {
-            calculator.inputCurrent(0);
-        }
-        if (source == B1) {
-            calculator.inputCurrent(1);
-        }
-        if (source == B2) {
-            calculator.inputCurrent(2);
-        }
-        if (source == B3) {
-            calculator.inputCurrent(3);
-        }
-        if (source == B4) {
-            calculator.inputCurrent(4);
-        }
-        if (source == B5) {
-            calculator.inputCurrent(5);
-        }
-        if (source == B6) {
-            calculator.inputCurrent(6);
-        }
-        if (source == B7) {
-            calculator.inputCurrent(7);
-        }
-        if (source == B8) {
-            calculator.inputCurrent(8);
-        }
-        if (source == B9) {
-            calculator.inputCurrent(9);
+        for (int i = 0; i <= 9; i++) {
+            if (source == B.get(i)) {
+                calculator.inputCurrent(i);
+            }
         }
         if (source == Bplus) {
             calculator.inputOperation('+');
